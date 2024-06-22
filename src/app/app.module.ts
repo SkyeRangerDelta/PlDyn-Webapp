1db8605af13af8fe3bf82a3416bf2ffc759faecb
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-06-22 00:46:26 -0400

    New: Added a fun button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgOptimizedImage } from "@angular/common";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatButton } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { NgOptimizedImage } from "@angular/common";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatButton
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
