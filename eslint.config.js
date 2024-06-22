46c245b45ea03a75316bbb767d4ef9cdf3019501
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-06-22 00:18:06 -0400

    Build: Added proper testing and linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,46 @@
 // @ts-check
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+// const eslint = require("@eslint/js");
+import eslint from "@eslint/js";
+// const tseslint = require("typescript-eslint");
+import tseslint from "typescript-eslint";
+// const angular = require("angular-eslint");
+import * as angular from "angular-eslint";
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
 );
