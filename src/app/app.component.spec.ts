b4916146f264fe70ff053c3490c0a622c5a02acd
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-06-22 00:18:47 -0400

    Fix: Fixed NgOptimizedImage directive

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NgOptimizedImage } from "@angular/common";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterModule.forRoot([]),
+        NgOptimizedImage
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PlDyn-Webapp'`, () => {
+  it(`should have as title 'PlDyn Dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PlDyn-Webapp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PlDyn-Webapp');
+    expect(app.title).toEqual('PlDyn Dashboard');
   });
 });
