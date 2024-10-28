5392a3a9e208ce3d2a79e3227cfee4c80af70691
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-10-28 17:47:48 -0400

    Fix: Repaired ENV handling

--- PlDyn_Webapp/main.ts
+++ PlDyn_Webapp/main.ts
@@ -3,29 +3,24 @@
 // Imports
 import { load } from '@std/dotenv';
 
-export function start() {
+export async function start() {
   // Vars
-  load( { export: true } );
+  await load( { export: true } );
 
-  const port = Deno.env.get('APP_PORT') || '4200';
+  const port = parseInt(Deno.env.get('APP_PORT') || '4200');
   const host = Deno.env.get('APP_HOST') || 'localhost';
 
-// Start the server
-  const cmd = `ng serve --host ${host} --port ${port}`;
+  console.log( `Starting server on ${host}:${port}` );
 
-  console.info('[INFO]', `Starting server on ${host}:${port}`);
-  const webapp = new Deno.Command(
-    cmd,
-    {
-      stdout: "piped",
-      stderr: "piped",
-    },
-  );
-
-  // const { code, stdout, stderr } = webapp.outputSync();
-  // console.error(stderr);
-  // console.log(stdout);
-  // console.log(code);
+  Deno.serve( { port: port, hostname: host }, reqHandler );
 }
 
-start();
+function reqHandler( req: Request ) {
+  const url = new URL( req.url );
+
+  console.log( `Request received: ${url.pathname}` );
+
+  return new Response( 'Hello World!', { status: 200 } );
+}
+
+await start();
