24c53c4ed477d4ba4e7f5feffdcb9667907b786b
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-08 17:18:37 -0500

    chore: Tweaked some Angular stuff

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 import { NgOptimizedImage } from "@angular/common";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatButton } from "@angular/material/button";
