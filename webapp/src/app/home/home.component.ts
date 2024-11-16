d82f3928637795e17ddb72af0b24cb29c0725141
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-16 14:34:58 -0500

    New: Added Home component

---
+++ webapp/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component( {
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  flagshipLogo = 'assets/logo.png';
+
+  generateRandomLink() {
+    const interestingSites = [
+      'https://www.wikipedia.org/',
+      'https://www.khanacademy.org/',
+      'https://www.ted.com/',
+      'https://archive.org/',
+      'https://www.space.com/',
+    ];
+    const randomIndex = Math.floor(Math.random() * interestingSites.length);
+    window.location.href = interestingSites[randomIndex];
+  }
+}
--- webapp/src/app/login/login.component.spec.ts
+++ webapp/src/app/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { MaterialModule } from '../material_module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [MaterialModule]
     })
     .compileComponents();
 
