88126ae70a44ae9f703fad6d8532276315f4246f
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-18 14:27:16 -0500

    New: Added dynamic route loader

---
+++ api/Routes/v1/API/status.ts
@@ -0,0 +1,36 @@
+import { Router, RouterContext } from '@oak/oak/router';
+import { humanizeBytes, humanizeTime } from "../../../Utilities/Formatters.ts";
+
+const router = new Router();
+
+router
+  .get('/status', ( ctx: RouterContext<string> ) => {
+    const appMemory = Deno.memoryUsage();
+    const systemMemory = Deno.systemMemoryInfo();
+    const uptime = Deno.osUptime();
+
+    ctx.response.body = {
+      hostname: Deno.hostname(),
+      appMemory: {
+        resident: appMemory.rss,
+        residentFormatted: humanizeBytes( appMemory.rss ),
+        heapTotal: Deno.memoryUsage().heapTotal,
+        heapTotalFormatted: humanizeBytes( appMemory.heapTotal ),
+        heapUsed: Deno.memoryUsage().heapUsed,
+        heapUsedFormatted: humanizeBytes( appMemory.heapUsed ),
+      },
+      systemMemory: {
+        total: systemMemory.total,
+        totalFormatted: humanizeBytes( systemMemory.total ),
+        free: Deno.systemMemoryInfo().free,
+        freeFormatted: humanizeBytes( systemMemory.free ),
+      },
+      osUptimeSeconds: uptime,
+      osUpTimeFormatted: humanizeTime( uptime ),
+    };
+  });
+
+export default {
+  name: 'Status',
+  router: router
+};
--- api/Routes/v1/JellyfinRouter.ts
+++ api/Routes/v1/JellyfinRouter.ts
@@ -1,14 +1,16 @@
 // api/Routes/JellyfinRouter.ts
 import { Router } from '@oak/oak/router';
+import { getRouter } from "./RouteLoader.ts";
 
-const JellyfinRouter = new Router();
+const JellyfinRouter: Router = await getRouter('Jellyfin');
 
 JellyfinRouter
   .get('/', (ctx) => {
     ctx.response.body = 'Jellyfin main route';
-  })
-  .get('/status', (ctx) => {
-    ctx.response.body = 'Jellyfin status';
   });
+
+for ( const route of JellyfinRouter.values() ) {
+  console.log( `[Router] [Jellyfin] Loaded ${ route.path } route` );
+}
 
 export { JellyfinRouter };
