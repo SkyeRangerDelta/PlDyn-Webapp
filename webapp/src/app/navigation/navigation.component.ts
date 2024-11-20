df9c014bb6516b7897b012582f2429f61009fe50
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-20 09:17:29 -0500

    New: Moved header/login content from app to nav

--- webapp/src/app/navigation/navigation.component.ts
+++ webapp/src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
 
+  authed: boolean = false;
+
+  constructor( private authService: AuthService, private router: Router ) {
+    this.authed = this.authService.isAuthenticated;
+  }
+
+  ngOnInit() {
+    this.authService.authState$.subscribe( (authed: boolean) => {
+      this.authed = authed;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  getUsername() {
+    return this.authService.getUsername();
+  }
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
 }
