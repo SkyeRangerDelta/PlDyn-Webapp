15110a26c4d89dccb64587fc61ea70768566b6d7
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-20 09:17:29 -0500

    New: Moved header/login content from app to nav

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 
 @Component( {
   selector: 'app-root',
@@ -10,23 +9,5 @@ export class AppComponent {
   title = 'PlDyn Dashboard';
   flagshipLogo = 'assets/logo.png';
 
-  authed: boolean = false;
-
-  constructor( private authService: AuthService ) {
-    this.authed = this.authService.isAuthenticated;
-  }
-
-  ngOnInit() {
-    this.authService.authState$.subscribe( (authed: boolean) => {
-      this.authed = authed;
-    });
-  }
-
-  logout() {
-    this.authService.logout();
-  }
-
-  getUsername() {
-    return this.authService.getUsername();
-  }
+  constructor() {}
 }
