330988b96566a0e28b70e4006b87a32299528940
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-21 16:13:02 -0500

    New: Added media dashboard component

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MediaDashboardComponent } from './media-dashboard/media-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'media', component: MediaDashboardComponent },
   { path: '**', redirectTo: '' }
 ];
 
