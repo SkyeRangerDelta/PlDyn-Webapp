7cf316067856fbbea51bae06947ace763224e70e
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-11-25 16:59:02 -0500

    Breaking: Shifted Auth to Main Router

--- api/Routes/MainRouter.ts
+++ api/Routes/MainRouter.ts
@@ -1,11 +1,60 @@
 // Main router handler
-import { Router } from '@oak/oak/router';
+import { Router, RouterContext } from '@oak/oak/router';
+import * as jose from 'https://deno.land/x/jose@v5.9.6/index.ts'
+
 import { APIRouter } from "./v1/APIRouter.ts";
 import { JellyfinRouter } from "./v1/JellyfinRouter.ts";
 
 const MainRouter = new Router();
 
+MainRouter.use( authMiddleware );
 MainRouter.use('/api/v1/jellyfin', JellyfinRouter.routes(), JellyfinRouter.allowedMethods());
 MainRouter.use('/api/v1', APIRouter.routes(), APIRouter.allowedMethods());
 
 export { MainRouter };
+
+async function authMiddleware( ctx: RouterContext<string>, next: () => Promise<unknown> ) {
+  const excludedRoutes = [
+    '/api/v1/auth',
+    '/api/v1/status'
+  ];
+
+  if ( excludedRoutes.includes( ctx.request.url.pathname ) ) {
+    await next();
+    return;
+  }
+
+  const token = ctx.request.headers.get('authorization')?.split(' ')[1];
+  if ( !token ) {
+    ctx.response.status = 401;
+    ctx.response.body = {
+      message: 'Unauthorized (no token)'
+    }
+
+    return;
+  }
+
+  try {
+    const secret = Deno.env.get('JWT_SECRET');
+    if ( !secret ) {
+      ctx.response.status = 500;
+      ctx.response.body = {
+        message: 'Internal Server Error'
+      }
+
+      console.warn('JWT_SECRET might not be set in environment!');
+
+      return;
+    }
+
+    await jose.jwtVerify( token, new TextEncoder().encode( secret ) );
+
+    await next();
+  }
+  catch {
+    ctx.response.status = 401;
+    ctx.response.body = {
+      message: 'Invalid token.'
+    }
+  }
+}
--- api/Routes/v1/API/GetTokenData.ts
+++ api/Routes/v1/API/GetTokenData.ts
@@ -4,32 +4,12 @@ import * as jose from 'https://deno.land/x/jose@v5.9.6/index.ts'
 const router = new Router();
 
 router
-  .post('/gettokendata', async ( ctx: RouterContext<string> ) => {
+  .post('/GetTokenData', async ( ctx: RouterContext<string> ) => {
 
     const token = ctx.request.headers.get('authorization')?.split(' ')[1];
     const secret = Deno.env.get('JWT_SECRET');
     const requestBody = await ctx.request.body.json();
     const requestedParams = requestBody.params;
-
-    if ( !token ) {
-      ctx.response.status = 401;
-      ctx.response.body = {
-        message: 'Unauthorized'
-      }
-
-      return;
-    }
-
-    if ( !secret ) {
-      ctx.response.status = 500;
-      ctx.response.body = {
-        message: 'Internal Server Error'
-      }
-
-      console.warn('JWT_SECRET might not be set in environment!');
-
-      return;
-    }
 
     if ( !requestBody || !requestedParams || requestedParams.length === 0 ) {
       ctx.response.status = 400;
@@ -42,7 +22,7 @@ router
 
     // Verify token here
     try {
-      const { payload } = await jose.jwtVerify( token, new TextEncoder().encode( secret ) );
+      const { payload } = await jose.jwtVerify( token!, new TextEncoder().encode( secret ) );
 
       ctx.response.body = {
         message: 'Success',
@@ -61,6 +41,6 @@ router
   });
 
 export default {
-  name: 'Status',
+  name: 'GetTokenData',
   router: router
 };
