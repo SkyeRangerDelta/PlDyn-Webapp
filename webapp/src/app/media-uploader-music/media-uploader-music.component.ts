92604647f1642d41ab7e3f4bfa14ff495b43fd05
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-12-23 12:23:42 -0500

    Update: Added form fields to uploader

--- webapp/src/app/media-uploader-music/media-uploader-music.component.ts
+++ webapp/src/app/media-uploader-music/media-uploader-music.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './media-uploader-music.component.scss'
 })
 export class MediaUploaderMusicComponent {
+  title: string = 'Music';
+  album: string = 'Album';
+  artist: string = 'Artist';
+  genre: string = 'Genre';
+  year: string = 'Year';
 
+  onFileChange( $event: Event ) {
+
+  }
+
+  uploadMusic() {
+
+  }
+
+  closeUploader() {
+
+  }
 }
