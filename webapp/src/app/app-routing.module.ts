d64419799aed1580e9ad29d71490cc28e0f502eb
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-12-23 12:14:18 -0500

    New: Added MediaUploaderMusic component

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MediaDashboardComponent } from './media-dashboard/media-dashboard.component';
+import { MediaUploaderMusicComponent } from './media-uploader-music/media-uploader-music.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'media', component: MediaDashboardComponent },
+  { path: 'music-editor', component: MediaUploaderMusicComponent },
   { path: '**', redirectTo: '' }
 ];
 
