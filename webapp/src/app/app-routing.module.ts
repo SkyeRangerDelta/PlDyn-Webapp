4c10be145b4074203b42a75be318349a7ffec379
SkyeRangerDelta <whutchison@boxlogixautomation.com>
2024-12-23 16:12:37 -0500

    Fix: Corrected routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'media', component: MediaDashboardComponent },
-  { path: 'media-uploader-music', component: MediaUploaderMusicComponent },
+  { path: 'music-editor', component: MediaUploaderMusicComponent },
   { path: '**', redirectTo: '' }
 ];
 
