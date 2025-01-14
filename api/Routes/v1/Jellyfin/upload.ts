08aa9dfb2421691d0ed4fecfbfd327fab0ebeb39
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-01-14 16:52:09 -0500

    Feat: Added data upload operations

--- api/Routes/v1/Jellyfin/upload.ts
+++ api/Routes/v1/Jellyfin/upload.ts
@@ -1,4 +1,5 @@
 import { Router } from '@oak/oak/router';
+import { ensureFolderExists } from "../../../Utilities/IOUtilities.ts";
 
 const router = new Router;
 
@@ -11,7 +12,29 @@ router.post('/upload', async (ctx) => {
 
   const formData = await ctx.request.body.formData();
 
-  console.log( 'Form data:', formData );
+  // Store the file data in a temp folder
+  // Ensure the folder exists
+  ensureFolderExists( Deno.cwd() + '/temp', 'audio-uploads' );
+
+  // Save the file data to the temp folder
+  const files = formData.getAll('files');
+  for ( const file of files ) {
+    if ( file instanceof File ) {
+      const fileData = new Uint8Array( await file.arrayBuffer() );
+      const filePath = new URL( `file://${Deno.cwd()}/temp/audio-uploads/${ file.name }` );
+
+      try {
+        Deno.writeFileSync( filePath, fileData );
+      }
+      catch {
+        console.error( 'Error writing file:', filePath, file.name );
+        ctx.response.status = 500;
+        ctx.response.body = { message: 'Error writing file' };
+      }
+    }
+  }
+
+  // Send the file data to Jellyfin server
 
   ctx.response.status = 200;
   ctx.response.body = { message: 'Done' };
