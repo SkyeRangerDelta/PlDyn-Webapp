723d17c28d457f2ad5b5f3f0bb970686203946fa
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-04-04 19:04:58 -0400

    fix: Unbroke the JWT logic again

--- api/Routes/v1/API/GetTokenData.ts
+++ api/Routes/v1/API/GetTokenData.ts
@@ -7,9 +7,18 @@ router
   .post('/GetTokenData', async ( ctx: RouterContext<string> ) => {
 
     const token = ctx.request.headers.get('authorization')?.split(' ')[1];
-    const secret = Deno.env.get('JWT_SECRET');
+    const secret = new TextEncoder().encode( Deno.env.get('JWT_SECRET') );
     const requestBody = await ctx.request.body.json();
     const requestedParams = requestBody.params;
+
+    if ( !token || token === 'null' ) {
+      ctx.response.status = 401;
+      ctx.response.body = {
+        message: 'Unauthorized (no token)'
+      }
+
+      return;
+    }
 
     if ( !requestBody || !requestedParams || requestedParams.length === 0 ) {
       ctx.response.status = 400;
@@ -22,7 +31,7 @@ router
 
     // Verify token here
     try {
-      const { payload } = await jose.jwtVerify( token!, new TextEncoder().encode( secret ) );
+      const { payload } = await jose.jwtVerify( token, secret );
 
       ctx.response.body = {
         message: 'Success',
@@ -32,10 +41,10 @@ router
         }, {})
       };
     }
-    catch {
+    catch ( decodeErr ) {
       ctx.response.status = 400;
       ctx.response.body = {
-        message: `Something's not gone right.`
+        message: `Something's not gone right.\n${ decodeErr }`
       }
     }
   });
